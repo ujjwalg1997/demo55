71609e31844300bb24ac6228658a48870b808a28
ujjwalg1997 <91521485+ujjwalg1997@users.noreply.github.com>
2021-09-28 14:18:47 +0530

    uploaded file

---
+++ hello.java
@@ -0,0 +1,14 @@
+package assignment1;
+
+public class hello {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+      System.out.println("Hello");
+      
+      System.out.println("UJJWAL GODSE");
+	
+	}
+
+}
